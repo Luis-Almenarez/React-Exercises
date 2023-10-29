80d5771efeddc84206af7ebc6a199bef316c9ba4
Luis Almenarez <luisalmenarez0428@gmail.com>
2023-10-29 18:53:11 -0500

    Initial Commit

---
+++ 00-Twitter-Follow-Cards/src/App.jsx
@@ -0,0 +1,36 @@
+import "./App.css";
+import { TwitterFollowCard } from "./TwitterFollowCard.jsx";
+
+const users = [
+  {
+    userName: "_luisalmenarez",
+    name: "Luis Almenarez",
+    isFollowing: true,
+  },
+  {
+    userName: "midudev",
+    name: "Miguel Ángel Durán",
+    isFollowing: true,
+  },
+  {
+    userName: "carlosazaustre",
+    name: "Carlos Azaustre 🎃",
+    isFollowing: false,
+  },
+  {
+    userName: "FalconMasters",
+    name: "Carlos Arturo",
+    isFollowing: false,
+  },
+];
+
+export function App() {
+  return (
+    <section className="App">
+      {users.map((user) => {
+        const { userName, name, isFollowing } = user;
+        return <TwitterFollowCard userName={userName} initialIsFollowing={isFollowing} name={name} key={userName} />;
+      })}
+    </section>
+  );
+}
